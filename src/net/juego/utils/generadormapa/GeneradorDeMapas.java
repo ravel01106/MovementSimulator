5c67166678afd8c08cd80b64cd7e525274e21927
ravel01106 <ravello2002@gmail.com>
2023-04-11 22:47:36 +0100

    fix: genera el mapa con los valores correspondientes

--- src/net/juego/utils/generadormapa/GeneradorDeMapas.java
+++ src/net/juego/utils/generadormapa/GeneradorDeMapas.java
@@ -3,7 +3,17 @@ package net.juego.utils.generadormapa;
 public class GeneradorDeMapas {
 
     public static String[][] generador (int tamanioMapa){
-        return new String[tamanioMapa][tamanioMapa];
+        String[][] mapaGenerado = new String[tamanioMapa][tamanioMapa];
+        for (int i = 0; i < mapaGenerado.length; i++) {
+            for (int j = 0; j < mapaGenerado.length; j++) {
+                if(i == 0 | i == mapaGenerado.length - 1 | j == 0 | j == mapaGenerado.length - 1){
+                    mapaGenerado[i][j] = "#";
+                }else{
+                    mapaGenerado[i][j] = "X";
+                }
+            }
+        }
+        return mapaGenerado;
     }
     public static void leyenda(){
         String msg = "Leyenda del mapa:\n";
