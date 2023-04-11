7ca6d031c0cf1ffc14eef63b821ffc231e02ec55
ravel01106 <ravello2002@gmail.com>
2023-04-11 10:52:31 +0100

    feat: añadir clase MenuDelJuego

---
+++ src/net/juego/comunicacion/menudejuego/MenuDelJuego.java
@@ -0,0 +1,10 @@
+package net.juego.comunicacion.menudejuego;
+
+import net.juego.mapa.Mapa;
+import net.juego.personaje.Personaje;
+
+public class MenuDelJuego {
+    private Personaje jugador;
+    private Mapa mapa;
+    
+}
